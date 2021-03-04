4101448fab84a722e384e42ee191f99a1d88ae32
Mika Vesasto <mika.vesasto@gmail.com>
2021-03-04 23:48:51 +0200

    Change the structure of inquirer questions

--- schemas/migrationItemList.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    type: 'list',
-    name: 'type',
-    message:
-      'What you would like to migrate from production environment to local?',
-    choices: ['Database', 'Theme', 'Plugins', 'Media files'],
-  },
-];
